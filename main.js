07902954ab23e8980f8ab7d392d30b5c513d55ae
Wojtek F <fedakwojciech@gmail.com>
2023-12-15 15:36:15 +0100

    corect fetch function and create HTMLelements for rest of the week

--- main.js
+++ main.js
@@ -1,65 +1,91 @@
 const API_KEY = "80544cd629934b5f910221101231212";
-
-const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Cracow`;
-// const URL = `http://api.weatherapi.com/v1/future.json?key=80544cd629934b5f910221101231212&q=Cracow&dt=2024-01-12`;
+const town = "Cracow";
+const URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${town}&days=7&aqi=no&alerts=no`;
 
 const WEEK = [
+  "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday",
 ];
+
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth();
+const year = today.getUTCFullYear();
+const dayOfTheWeek = today.getDay(); //tu sa numery dni tygodnia poniedziałek
+
+const createDayElement = (dayName, date, temperature) => {
+  const newDayContent = document.createTextNode(dayName);
+  const newParagraphDay = document.createElement("p");
+  newParagraphDay.appendChild(newDayContent);
+
+  const newDateContent = document.createTextNode(date);
+  const newParagraphDate = document.createElement("p");
+  newParagraphDate.appendChild(newDateContent);
+
+  const newTemperatureContent = document.createTextNode(`${temperature}°C`);
+  const newParagraphTemp = document.createElement("p");
+  newParagraphTemp.appendChild(newTemperatureContent);
+
+  const newImageSvg = document.createElement("img");
+  newImageSvg.setAttribute("src", "assetes/svgFile/raindSvg.svg");
+
+  const newDivContainer = document.createElement("div");
+  newDivContainer.classList.add("tomorrow");
+  newDivContainer.appendChild(newImageSvg);
+  newDivContainer.appendChild(newParagraphDay);
+  newDivContainer.appendChild(newParagraphDate);
+  newDivContainer.appendChild(newParagraphTemp);
+
+  elementMother.appendChild(newDivContainer);
+};
 
 const getTodayAPI = async () => {
   const response = await fetch(URL);
   const data = await response.json();
-  // console.log("dzis", data);
 
   const temperatureElement = document.querySelector(".today-temperature");
   temperatureElement.textContent = `${data.current.temp_c}°C`;
 };
 
-const getTomorowAPI = async (town, date) => {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/future.json?key=${API_KEY}&q=${town}&dt=${date}`
-  );
+const getWeekAPI = async () => {
+  const response = await fetch(URL);
   const data = await response.json();
 
-  const tomorowDayOfWeekElement = document.querySelector(".tomorow-text-day");
-  const tomorowDateElement = document.querySelector(".tomorow-text-date");
-  const tomorowTemperatureElement = document.querySelector(
-    ".tomorow-text-temperature"
-  );
-  tomorowDayOfWeekElement.textContent = `Piątek`;
-  tomorowDateElement.textContent = `${data.forecast.forecastday[0].date}`;
-  tomorowTemperatureElement.textContent = `${data.forecast.forecastday[0].day.avgtemp_c}°C`;
+  const weekData = data.forecast.forecastday.map((day, index) => {
+    const isEndOfWeek = dayOfTheWeek + index > 6;
+    const nameOfDay = isEndOfWeek
+      ? dayOfTheWeek + index - 7
+      : dayOfTheWeek + index;
+    return {
+      date: day.date,
+      nameOfDay: WEEK[nameOfDay],
+      avgTemp: day.day.avgtemp_c,
+    };
+  });
 
-  console.log("jutro", data);
-  // console.log("jutro", data.forecast.forecastday[0].day.avgtemp_c);
-  // console.log("jutro", data.forecast.forecastday[0].date);
+  weekData.forEach((day, index) => {
+    if (index > 0) {
+      createDayElement(day.nameOfDay, day.date, day.avgTemp);
+    } else {
+      return;
+    }
+  });
 };
+
+getWeekAPI();
 
 getTodayAPI();
 
-getTomorowAPI("Cracow", "2024-01-12");
-
 const getToddayInformation = () => {
-  const today = new Date();
-  const day = today.getDate();
-  const month = today.getMonth();
-  const year = today.getUTCFullYear();
-  const dayOfTheWeek = today.getDay();
-  // const hours = today.getHours();
-  // const minutes = today.getMinutes();
-
   const dayElement = document.querySelector(".todday-day");
-  // const timeElement = document.querySelector(".todday-time");
-
-  dayElement.textContent = ` ${day}.${month}.${year} ${WEEK[dayOfTheWeek]}`;
-  // timeElement.textContent = `${hours} : ${minutes}`;
+  dayElement.textContent = ` ${day}-${month}-${year} ${WEEK[dayOfTheWeek]}`;
 };
 
 getToddayInformation();
+
+const elementMother = document.querySelector(".week-container");
